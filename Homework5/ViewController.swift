f7713ae26194d1b385906d3de71afa52450d7166
ezgiik <ezgiikarabay@hotmail.com>
2023-09-20 10:34:35 +0300

    Initial Commit

---
+++ Homework5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework5
+//
+//  Created by EZGİ KARABAY on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
